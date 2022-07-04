812b3a12c652bce700536c156094fe78c9913740
“fariahatulmaria” <89478001+farihatulmaria@users.noreply.github.com>
2022-07-04 16:58:56 +0600

    update

--- red-store/src/App.js
+++ red-store/src/App.js
@@ -1,10 +1,14 @@
+import { Route, Routes } from 'react-router-dom';
 import Header from "./Components/Header";
-
+import Home from "./Pages/Home";
 function App() {
   return (
-    <>
+    <div className="max-w-[1980px] mx-auto">
       <Header/>
-    </>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+      </Routes>
+    </div>
   );
 }
 
--- red-store/src/Components/Header.jsx
+++ red-store/src/Components/Header.jsx
@@ -1,30 +1,31 @@
 import { faShoppingBag } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import logo from '../assets/img/logo.png';
 const Header = () => {
     const menuItems = <>
-        <li><a href="/">Home</a></li>
-        <li><a href="/products">Products</a></li>
-        <li><a href="/about">About</a></li>
-        <li><a href="/contact">Contact</a></li>
-        <li><a href="/profile">Profile</a></li>
-        <li><a href="/cart"><FontAwesomeIcon icon={faShoppingBag} /></a></li>
+        <li><NavLink to={'/'}>Home</NavLink></li>
+        <li><NavLink to={"/products"}>Products</NavLink></li>
+        <li><NavLink to={"/about"}>About</NavLink></li>
+        <li><NavLink to={"/contact"}>Contact</NavLink></li>
+        <li><NavLink to={"/profile"}>Profile</NavLink></li>
+        <li><NavLink to={"/cart"}><FontAwesomeIcon icon={faShoppingBag} /></NavLink></li>
     </>
     return (
-        <header>
-            <div className="navbar bg-base-100">
+        <header className='my-3 mx-4'>
+            <nav className="navbar bg-base-100 items-center">
                 <div className="navbar-start">
                     <a href="/" className="btn btn-ghost normal-case text-xl">
-                        <img src={logo} alt="" />
+                        <img src={logo} alt="" width={'200px'}/>
                     </a>
                 </div>
                 <div className="navbar-end">
                     <div className="dropdown dropdown-end">
-                        <label tabindex="0" className="btn btn-ghost lg:hidden">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        <label tabIndex="0" className="btn btn-primary text-white lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </label>
-                        <ul tabindex="0" className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        <ul tabIndex="0" className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
                             {menuItems}
                         </ul>
                     </div>
@@ -35,7 +36,7 @@ const Header = () => {
                     </div>
                     
                 </div>
-            </div>  
+            </nav>  
         </header>
     );
 };
