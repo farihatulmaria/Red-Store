fe21e775e261ed16062c9aa730294ae60513daa6
“fariahatulmaria” <89478001+farihatulmaria@users.noreply.github.com>
2022-07-05 11:34:16 +0600

    thinking about how to setup the featured products

--- red-store/src/Components/FeaturedProducts.jsx
+++ red-store/src/Components/FeaturedProducts.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import product1 from '../assets/img/product-1.jpg';
 import Title from './Title';
-
 const FeaturedProducts = () => {
     return (
         <div className='featured-products'>
             <Title name={'Featured Products'}/>
+            <div className="small-container">
+                <div className="product">
+                    <img src={product1} alt="" className='w-60' />
+                </div>
+            </div>
         </div>
     );
 };
--- red-store/src/Components/Title.jsx
+++ red-store/src/Components/Title.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 
 const Title = ({name}) => {
     return (
-        <div className="title text-center">
+        <div className="title text-center my-6">
             <h2>{name}</h2>
+            <div className="underline"></div>
         </div>
     );
 };
